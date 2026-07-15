d390d246b12a16cba31388fa75a5eca3325e5411
Carles Vila Gelada <litusvila199@gmail.com>
2026-07-15 17:23:52 +0200

    feat:Transform done succefully

--- src/transform.py
+++ src/transform.py
@@ -1,15 +1,51 @@
-import pandas as pd
 import json
+from pathlib import Path
 
-with open("data/raw/barcelona.json", "r", encoding="utf-8") as f:
-    data = json.load(f)
+import pandas as pd
 
-hourly = data["hourly"]
 
-df = pd.DataFrame(hourly)
+def transform_weather():
+    files = Path("data/raw").glob("*.json")
 
-print(df.head())
+    dfs = []
 
-df["time"] = pd.to_datetime(df["time"])
+    for file in files:
+        with open(file, "r", encoding="utf-8") as f:
+            data = json.load(f)
 
-print(df.info())
+        hourly = data["hourly"]
+
+        df = pd.DataFrame(hourly)
+
+        # Transformaciones
+        df["time"] = pd.to_datetime(df["time"])
+        df["city"] = file.stem.capitalize()
+
+        df.rename(
+            columns={
+                "temperature_2m": "temperature",
+                "relative_humidity_2m": "humidity",
+                "wind_speed_10m": "wind_speed",
+            },
+            inplace=True,
+        )
+
+        # Guardamos el DataFrame de esa ciudad
+        dfs.append(df)
+
+    # Unimos todos los DataFrames
+    df_final = pd.concat(dfs, ignore_index=True)
+
+    return df_final
+
+
+def main():
+    df = transform_weather()
+
+    print(df.head())
+    print()
+    print(df.info())
+
+
+if __name__ == "__main__":
+    main()
